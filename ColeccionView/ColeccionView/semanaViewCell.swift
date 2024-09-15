e0e4d5e6af3a548b10c433aa7e3ae5f9b403b72d
PGallo <patogallo95@gmail.com>
2024-09-14 21:12:41 -0300

    Subo los ultimos cambios

---
+++ ColeccionView/ColeccionView/semanaViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  semanaViewCell.swift
+//  ColeccionView
+//
+//  Created by Pato Gallo on 14/09/2024.
+//
+
+import UIKit
+
+class semanaViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
