aa0664424a74c9fbd6daa77865290ed54275c309
PGallo <patogallo95@gmail.com>
2024-09-16 18:07:34 -0300

    Agrego cambios en la ultima vista y funcion para calculo de porcentaje

---
+++ ColeccionView/ColeccionView/ejercicioTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ejercicioTableViewCell.swift
+//  ColeccionView
+//
+//  Created by Pato Gallo on 16/09/2024.
+//
+
+import UIKit
+
+class ejercicioTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ColeccionView/ColeccionView/ejerciciosViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ejerciciosViewCell.swift
+//  ColeccionView
+//
+//  Created by Pato Gallo on 16/09/2024.
+//
+
+import UIKit
+
+class ejerciciosViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
